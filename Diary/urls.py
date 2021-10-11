3d27916c6ff337157475cc5a6d23f8530798f942
ReinhardyOdhiambo <reinhardy.odhiambo@gmail.com>
2021-10-12 01:27:55 +0300

    Added files and changes required for deployment to heroku

--- Diary/urls.py
+++ Diary/urls.py
@@ -18,9 +18,10 @@ from django.urls import path, include
 from django.views.generic import RedirectView
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('accounts/', include('allauth.urls')),
-    path('', include('Mydiary.urls'))
+    path('', include('Mydiary.urls')),
+    path('admin/', admin.site.urls)
+
+
 
    
 ]
