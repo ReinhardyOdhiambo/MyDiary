dfa86b0f1df8758f7b54cb235af7128fd470e002
ReinhardyOdhiambo <reinhardy.odhiambo@gmail.com>
2021-10-11 17:44:47 +0300

    Added files and changes required for deployment to heroku

--- Diary/settings.py
+++ Diary/settings.py
@@ -27,9 +27,7 @@ SECRET_KEY = os.environ.get('DJANGO_SECRET_KEY', ')wnt@!(m%a4(ddh72(8v#prl3g74f0
 DEBUG = os.environ.get('DJANGO_DEBUG', '') != 'False'
 
 ALLOWED_HOSTS = [
-    '0.0.0.0'
-    'secure-depths-59571.herokuapp.com',
-    '127.0.0.1'
+    'secure-depths-59571.herokuapp.com'
 ]
 
 
